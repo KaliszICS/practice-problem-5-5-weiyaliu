8f5a35fbf1348e4495f535653652bcdfc9b0f7dc
weiyaliu <343445839@tdsb.ca>
2026-05-09 02:47:48 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,4 +4,23 @@ public class PracticeProblem {
 
 	}
 
+	public static boolean isPalindrome (String str) {
+
+		if (str.length() == 0) { 
+        return true;
+
+		}
+
+		str= str.replace(" ","").toLowerCase();
+
+		for (int i = 0; i < str.length(); i++) {
+
+			if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+
 }
